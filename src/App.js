287647e70341e990cd13f793835002ac5a5e10ce
Juli <Juli@Digitals-MacBook-Pro.local>
2020-05-20 13:23:11 +0300

    privet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Reset } from 'reset-css';
+import Leaf from './Leaf.svg'
+
+
+import * as S from './style.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <S.Hero></S.Hero>
+      <img src={Leaf} />
+      <S.Benefits>
+        <S.Caption>
+          <S.TextBold>Why MyFit?</S.TextBold>
+          <S.CaptionText>We are a UK company looking to revolutionise the way we workout! Resistance bands are cool - but they’re a lot cooler when they’ve been created and designed to allow you to replicate nearly all gym workouts. We’ve managed to create resistance from the comfort of your own home without compromising on your workout goals.</S.CaptionText>
+        </S.Caption>
+        <S.Content>
+          <S.Card>
+            <S.CardText>If you are allergic to latex, we would advise consulting.</S.CardText>
+          </S.Card>
+          <S.Card>
+            <S.CardText>For the 2nd one, can you create a symbol that represents a phone, that box will be used to talk about the app.</S.CardText>
+          </S.Card>
+          <S.Card>
+            <S.CardText>the symbol should be related to quality control, - represents ‘number 1’.</S.CardText>
+          </S.Card>
+        </S.Content>
+      </S.Benefits>
+      <S.VideoBlock></S.VideoBlock>
+      <S.App></S.App>
+      <S.Bottom></S.Bottom>
+      <S.Footer></S.Footer>
     </div>
   );
 }
---
+++ src/colors.js
@@ -0,0 +1,13 @@
+export const PRIMARY = '#FA4504'
+
+export const BLACK = '#000000'
+export const WHITE = '#FFFFFF'
+export const DARK_GREY = '#5B5B5B'
+export const GREY = '#BDBDBD'
+export const LIGHT_DARK = '#E9FFF7'
+
+export const SOFT_ORANGE = '#FFEBE0'
+export const SOFT_BLUE = '#D6EDFF'
+export const SOFT_PINK = '#FFEFF4'
+export const SOFT_YELLOW = '#FFF8E1'
+export const SOFT_GREEN = '#E9FFF7'
